48df542c3eeb2fe6dbf1c9d6f697ce34c6d35a14
Michael Walshe <michael.walshe@amadeus.co.uk>
2022-12-01 10:33:47 +0000

    update

--- aoc-python/support/support.py
+++ aoc-python/support/support.py
@@ -4,7 +4,6 @@ import argparse
 import contextlib
 import enum
 import os.path
-import re
 import sys
 import time
 import urllib.error
@@ -48,17 +47,6 @@ def get_input(year: int, day: int) -> str:
     return urllib.request.urlopen(req).read().decode()
 
 
-def get_year_day() -> tuple[int, int]:
-    cwd = os.getcwd()
-    day_s = os.path.basename(cwd)
-    year_s = os.path.basename(os.path.dirname(cwd))
-
-    if not day_s.startswith('day') or not year_s.startswith('advent-of-code'):
-        raise AssertionError(f'unexpected working dir: {cwd}')
-
-    return int(year_s[len('advent-of-code'):]), int(day_s[len('day'):])
-
-
 def download_input(day, daypath) -> int:
     parser = argparse.ArgumentParser()
     parser.parse_args()
@@ -98,69 +86,6 @@ def create_day(day):
     copy_tree(str(ROOT / "day00"), str(ROOT / day_name))
 
     download_input(day, ROOT / day_name)
-
-
-TOO_QUICK = re.compile('You gave an answer too recently.*to wait.')
-WRONG = re.compile(r"That's not the right answer.*?\.")
-RIGHT = "That's the right answer!"
-ALREADY_DONE = re.compile(r"You don't seem to be solving.*\?")
-
-
-def _post_answer(year: int, day: int, part: int, answer: int) -> str:
-    params = urllib.parse.urlencode({'level': part, 'answer': answer})
-    req = urllib.request.Request(
-        f'https://adventofcode.com/{year}/day/{day}/answer',
-        method='POST',
-        data=params.encode(),
-        headers=_get_cookie_headers(),
-    )
-    resp = urllib.request.urlopen(req)
-
-    return resp.read().decode()
-
-
-def submit_solution() -> int:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--part', type=int, required=True)
-    args = parser.parse_args()
-
-    year, day = get_year_day()
-    answer = int(sys.stdin.read())
-
-    print(f'answer: {answer}')
-
-    contents = _post_answer(year, day, args.part, answer)
-
-    for error_regex in (WRONG, TOO_QUICK, ALREADY_DONE):
-        error_match = error_regex.search(contents)
-        if error_match:
-            print(f'\033[41m{error_match[0]}\033[m')
-            return 1
-
-    if RIGHT in contents:
-        print(f'\033[42m{RIGHT}\033[m')
-        return 0
-    else:
-        # unexpected output?
-        print(contents)
-        return 1
-
-
-def submit_25_pt2() -> int:
-    parser = argparse.ArgumentParser()
-    parser.parse_args()
-
-    year, day = get_year_day()
-
-    assert day == 25, day
-    contents = _post_answer(year, day, part=2, answer=0)
-
-    if 'Congratulations!' in contents:
-        print('\033[42mCongratulations!\033[m')
-        return 0
-    else:
-        print(contents)
-        return 1
 
 
 def adjacent_4(x: int, y: int) -> Generator[tuple[int, int], None, None]:
